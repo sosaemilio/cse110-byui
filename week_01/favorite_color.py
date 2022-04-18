90f27574996b3f31a05e67b8d739922bb5a6e994
Emilio Sosa <45885030+sosaemilio@users.noreply.github.com>
2022-04-18 00:50:46 -0400

    Uploaded Week 01 Folder
    Week 01 Checkpoint code is included and the milestone is as well.

---
+++ week_01/favorite_color.py
@@ -0,0 +1,6 @@
+#Ask for the color and save it in a variable
+favorite_color = input("What is your favorite color: ")
+
+#Show in the console the favorite color
+print("Your favorite color is", favorite_color, "!")
+print(favorite_color)
